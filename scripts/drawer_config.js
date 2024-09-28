2b6ce5cf6bf7624229b858b1e42c61f9e8cf40eb
Isabela Rodrigues <bela.rodjj@gmail.com>
2024-09-28 14:52:05 -0300

    adasds

--- scripts/drawer_config.js
+++ scripts/drawer_config.js
@@ -1,11 +1,15 @@
 //BOTAO PARA ABRIR ABA
 
-const toggleConfig = document.getElementById("nav__link--click")
-const aba = document.getElementById("troca__tema")
+const toggleConfig = document.getElementById("nav__link--click");
+const aba = document.getElementById("troca__tema");
 
 toggleConfig.addEventListener('click', () => {
-  if (aba.style.display === 'none') {
-    aba.style.display = 'block'; // Mostra a abinha
-} else {
-    aba.style.display = 'none'; // Esconde a abinha
-}})
+  // Verifica se a aba tem a classe 'hidden'
+  if (aba.classList.contains('hidden')) {
+    aba.classList.remove('hidden');  // Remove a classe 'hidden' para mostrar
+    aba.classList.add('visible');    // Adiciona a classe 'visible'
+  } else {
+    aba.classList.remove('visible'); // Remove a classe 'visible'
+    aba.classList.add('hidden');     // Adiciona a classe 'hidden' para esconder
+  }
+});
