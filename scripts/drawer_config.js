d25fc0fed8cf635e5e7a0b45246032f4bb57bf24
Isabela Rodrigues <bela.rodjj@gmail.com>
2024-09-29 17:04:34 -0300

    speakers

--- scripts/drawer_config.js
+++ scripts/drawer_config.js
@@ -2,12 +2,13 @@
 
 const toggleConfig = document.getElementById("nav__link--click");
 const aba = document.getElementById("troca__tema");
+const icon = document.querySelector('nav__link--click')
 
 toggleConfig.addEventListener('click', () => {
   // Verifica se a aba tem a classe 'hidden'
   if (aba.classList.contains('hidden')) {
     aba.classList.remove('hidden');  // Remove a classe 'hidden' para mostrar
-    aba.classList.add('visible');    // Adiciona a classe 'visible'
+    aba.classList.add('visible');   // Adiciona a classe 'visible'
   } else {
     aba.classList.remove('visible'); // Remove a classe 'visible'
     aba.classList.add('hidden');     // Adiciona a classe 'hidden' para esconder
